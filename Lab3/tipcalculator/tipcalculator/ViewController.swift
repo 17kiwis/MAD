49e2e109c004118df8c7daa8d2a9bddcabb97143
Emma Wu <shwu3484@colorado.edu>
2016-09-26 21:19:23 -0600

    Lab3

---
+++ Lab3/tipcalculator/tipcalculator/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Emma Wu on 9/20/16.
+//  Copyright © 2016 Emma Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var checkAmount: UITextField!
+    @IBOutlet weak var tipPercent: UITextField!
+    @IBOutlet weak var people: UITextField!
+    @IBOutlet weak var tipDue: UILabel!
+    @IBOutlet weak var totalDue: UILabel!
+    @IBOutlet weak var totalDuePerPerson: UILabel!
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func updateTipTotals() {
+        var amount:Float // amount=check amount
+        var pct:Float //pct=tip percentage
+        
+        if checkAmount.text!.isEmpty {
+            amount = 0.0
+        } else {
+            amount = Float(checkAmount.text!)!
+        }
+        if tipPercent.text!.isEmpty {
+            pct = 0.0
+        }
+        else {
+            pct = Float(tipPercent.text!)!/100
+        }
+        let numberOfPeople=Int(people.text!) //will returns an optional
+        let tip=amount*pct
+        let total=amount+tip
+        var personTotal : Float = 0.0
+        if numberOfPeople != nil {
+            if numberOfPeople! > 0 {
+                personTotal = total / Float(numberOfPeople!)
+            }
+            else {
+                
+                let alert=UIAlertController(title: "Warning", message: "The number of people must be greater than 0", preferredStyle: UIAlertControllerStyle.Alert)
+                    let cancelAction=UIAlertAction(title: "Cancel", style:UIAlertActionStyle.Cancel, handler: nil)
+                alert.addAction(cancelAction)
+                let okAction=UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {action in
+                    self.people.text="1"
+                    self.updateTipTotals()
+                })
+                alert.addAction(okAction)
+                presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+
+        //put it as currency
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle=NSNumberFormatterStyle.CurrencyStyle
+        tipDue.text=currencyFormatter.stringFromNumber(tip)
+        totalDue.text=currencyFormatter.stringFromNumber(total)
+        totalDuePerPerson.text=currencyFormatter.stringFromNumber(personTotal)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        updateTipTotals()
+    }
+    
+    override func viewDidLoad() {
+        checkAmount.delegate=self
+        tipPercent.delegate=self
+        people.delegate=self
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
