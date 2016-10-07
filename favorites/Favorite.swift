3b47451a4cf5a50ba9cde08dbc1b95b7069eaf71
Emma Wu <shwu3484@colorado.edu>
2016-10-07 11:38:43 -0600

    Add files via upload

---
+++ favorites/Favorite.swift
@@ -0,0 +1,16 @@
+//
+//  Favorite.swift
+//  favorites
+//
+//  Created by Emma Wu on 10/7/16.
+//  Copyright © 2016 Emma Wu. All rights reserved.
+//
+
+import Foundation
+
+class Favorite {
+    var favBook : String?
+    var favAuthor : String?
+    var favType : String?
+    
+}
